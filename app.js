864013c8d31311701aa2bb5198d3de7445966ea8
L-ONE-APP <Dogmanenok227@yandex.ru>
2025-09-12 11:05:26 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-window.onload = function() {
-    
-};
+const TG = window.Telegram.WebApp;
+
+const user = TG.initDataUnsafe.user;
+
+if (user) {
+    const userId = user.id;
+    const firstName = user.firest_name;
+    const lastName = user.last_name || '';
+    const photoUrl = user.photo_url || '';
+    const username = user.username || '';
+
+    const UserInfo = document.getElementById('info');
+    UserInfo.innerHTML = `<p><strong>Имя:</strong>@${username}</p>`
+}
