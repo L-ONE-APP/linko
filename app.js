166ecf463c3ab4f1f72806628572bcef30e56610
L-ONE-APP <Dogmanenok227@yandex.ru>
2025-09-12 12:04:52 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ const div = document.getElementById('username');
 
 const username = user.username || '';
 
+window.WebApp.requestFullscreen();
+
 div.innerHTML = `
 <div class="cls"><p class="txt">Username: ${username}</p></div>`
