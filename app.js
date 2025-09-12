011ba38744251f0fcc3a4e77400ac46ee5ab20d4
L-ONE-APP <Dogmanenok227@yandex.ru>
2025-09-12 11:35:16 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const TG = window.Telegram.WebApp;
-
 const user = TG.initDataUnsafe.user;
 
-if (user) {
-    const userId = user.id;
-    const firstName = user.firest_name;
-    const lastName = user.last_name || '';
-    const photoUrl = user.photo_url || '';
-    const username = user.username || '';
+const div = document.getElementById('username');
 
-    const UserInfo = document.getElementById('info');
-    UserInfo.innerHTML = `<p><strong>Имя:</strong>@${username}</p>`
-}
+const username = user.username || '';
+
+div.innerHTML = `
+<div class="cls"><p class="txt">Username: ${username}</p></div>`
