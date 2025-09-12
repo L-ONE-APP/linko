6aaf8de629d71cdb85a3d941c74e17d2b4cbb719
L-ONE-APP <Dogmanenok227@yandex.ru>
2025-09-12 12:34:52 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const div = document.getElementById('username');
 const username = user.username || '';
 
 Telegram.WebApp.expand();
-Telegram.WebApp.requestFullscreen();
 
 div.innerHTML = `
 <div class="cls"><p class="txt">Username: ${username}</p></div>`
